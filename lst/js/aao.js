ed1beb685981a01b903b865eeaff728fbed82d26
Daniel Baldauf <81148839+adler10@users.noreply.github.com>
2025-11-17 11:16:50 +0100

    Create aao.js

---
+++ lst/js/aao.js
@@ -0,0 +1,12 @@
+// Compact AAO suggestions array exported to window for UI autocomplete usage
+window. AAO_SUGGESTIONS = [
+"TH - Ast / Baum droht zu fallen",
+"TH - Ast / Baum entfernen",
+"TH - Fahrbahn reinigen / aufräumen",
+"B - Kleinfeuer",
+"B= PKW / Kleinbus innerorts",
+"S - First Responder",
+"B - Auslösung BMA",
+"TH - Wasserschaden",
+"TH - Person im Wasser"
+  ];
